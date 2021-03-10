fde8adc9b6eaed2ef0053e6a80c0415348c3a1d4
Hamza Ashour <hamza.ashour.work@gmail.com>
2021-03-10 22:51:25 +0200

    added 4.3 tests

--- src/EventList.js
+++ src/EventList.js
@@ -1,12 +1,19 @@
 // src/EventList.js
 
 import React, { Component } from 'react';
+import Event from './Event'; // in src/EventList.js
 
 class EventList extends Component {
   render() {
+    const { events } = this.props;
     return (
-      <div>
-      </div>
+      <ul className="EventList">
+        {events.map(event =>
+          <li key={event.id}>
+            <Event event={event} />
+          </li>
+        )}
+      </ul>
     );
   }
 }
